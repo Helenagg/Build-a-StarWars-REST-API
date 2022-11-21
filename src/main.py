563c35b89c820f3e9f28e8ec6667548fb47904f0
Helenagg <hgonzalezg18@gmail.com>
2022-11-21 20:26:21 +0000

    endpoint create

--- src/main.py
+++ src/main.py
@@ -20,6 +20,60 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+people = [{"id": 1,
+        "name": "Luke Skywalker", 
+        "height": "172", 
+        "mass": "77", 
+        "hair_color": "blond", 
+        "skin_color": "fair", 
+        "eye_color": "blue", 
+        "birth_year": "19BBY"},
+
+    {"id": 2,
+    "name": "C-3PO", 
+    "height": "167", 
+    "mass": "75", 
+    "hair_color": "n/a", 
+    "skin_color": "gold", 
+    "eye_color": "yellow", 
+    "birth_year": "112BBY"}]
+
+planets = [
+    {"id":0,
+    "name": "Tatooine", 
+    "rotation_period": "23", 
+    "orbital_period": "304", 
+    "diameter": "10465", 
+    "climate": "arid", 
+    "gravity": "1 standard", 
+    "terrain": "desert", 
+    "surface_water": "1", 
+    "population": "200000"},
+    {"id":1,
+    "name": "Alderaan", 
+    "rotation_period": "24", 
+    "orbital_period": "364", 
+    "diameter": "12500", 
+    "climate": "temperate", 
+    "gravity": "1 standard", 
+    "terrain": "grasslands, mountains", 
+    "surface_water": "40", 
+    "population": "2000000000"}
+]
+
+users = [
+    {"id": 1,
+  "first_name": "Bob",
+  "last_name": "Dylan",
+  "email": "bob@dylan2.com",
+  "password": "asda"},
+  {"id": 2,
+  "first_name": "Paul",
+  "last_name": "McCartney",
+  "email": "paul@dmccartney.com",
+  "password": "bcbc"}
+]
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -38,6 +92,31 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def handle_people():
+    response_people = people
+    return jsonify(response_people), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def select_people(people_id):
+    person = list(filter(lambda element: element['id'] == people_id, people))
+    return jsonify(person), 200
+
+@app.route('/planet', methods=['GET'])
+def handle_planet():
+    response_planet = planets
+    return jsonify(response_planet), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def select_planet(planet_id):
+    planet = list(filter(lambda element: element['id'] == planet_id, planets))
+    return jsonify(planet), 200
+
+@app.route('/users', methods=['GET'])
+def handle_user():
+    response_user = users
+    return jsonify(response_user), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
