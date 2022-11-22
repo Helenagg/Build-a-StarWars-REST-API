a1319501cfd6d022af8ecc8542f64650f031dd6f
Helenagg <hgonzalezg18@gmail.com>
2022-11-22 11:43:21 +0000

    endponit planet people and users

--- src/main.py
+++ src/main.py
@@ -63,15 +63,26 @@ planets = [
 
 users = [
     {"id": 1,
-  "first_name": "Bob",
-  "last_name": "Dylan",
-  "email": "bob@dylan2.com",
-  "password": "asda"},
-  {"id": 2,
-  "first_name": "Paul",
-  "last_name": "McCartney",
-  "email": "paul@dmccartney.com",
-  "password": "bcbc"}
+    "first_name": "Bob",
+    "last_name": "Dylan",
+    "email": "bob@dylan2.com",
+    "password": "asda"},
+    {"id": 2,
+    "first_name": "Paul",
+    "last_name": "McCartney",
+    "email": "paul@dmccartney.com",
+    "password": "bcbc"}
+]
+
+favorites = [
+    {"id": 1,
+    "id_user": 1,
+    "id_planet": 1,
+    "id_people": 1},
+    {"id": 2,
+    "id_user": 2,
+    "id_planet": 1,
+    "id_people": 1}
 ]
 
 # Handle/serialize errors like a JSON object
@@ -117,6 +128,21 @@ def select_planet(planet_id):
 def handle_user():
     response_user = users
     return jsonify(response_user), 200
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def select_user(user_id):
+    user = list(filter(lambda element: element['id'] == user_id, users))
+    return jsonify(user), 200
+
+@app.route('/favorite', methods=['GET'])
+def handle_fav():
+    response_fav = favorites
+    return jsonify(response_fav), 200
+
+@app.route('/favorite/<int:fav_id>', methods=['GET'])
+def select_fav(fav_id):
+    favorite = list(filter(lambda element: element['id'] == fav_id, favorites))
+    return jsonify(favorite), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
